0c5fe0228dbc5ef02af77ce49246cdc885d5a8d4
Wily Fox Technologies <rdongre@wilyfox.com>
2016-06-02 22:32:46 +0000

    First commit

---
+++ Login.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta content="IE=11.0000" http-equiv="X-UA-Compatible">
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Child Welfare Services: Case Management & Residential Licensing System">
+    <meta name="author" content="Wily Fox Technologies">
+
+    <title>Child Welfare Services: Case Management & Residential Licensing System</title>
+    <link href="/images/CWDS.png" rel="shortcut icon"/>
+
+    <link rel="stylesheet" href="Content/custom.css"/>
+    <link href="Content/Portal.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="Content/bootstrap.min.css"/>
+    <link href="Content/jquery-ui.min.css" rel="stylesheet"/>
+    <link href="css/font-awesome.min.css" rel="stylesheet"/>
+
+    <script src="Scripts/jquery-1.10.2.min.js" crossorigin="anonymous"></script>
+    <script src="Scripts/bootstrap.min.js" crossorigin="anonymous"></script>
+    <script src="Scripts/jquery-ui-1.11.4.min.js"></script>
+    <script src="Scripts/portal.custom.js"></script>
+    <script src="Scripts/Global.js"></script>
+</head>
+<body class="body-blue">
+<div id="container">
+    <div class="mini-navbar mini-navbar-dark hidden-xs" id="header">
+        <div class="customheader">
+            <div class="container-fluid" style="padding-left:5px;">
+                <div class="row no-gutters">
+                    <div class="col-lg-9 col-sm-6 col-md-8 text-left">
+                        <div class="apptitle">
+                            <div style="width:70px;float: left;">
+                                <img style="max-height: 70px;padding-top: 5px;" src="images/cwds.png"/>
+                            </div>
+                            <div style="max-width: 400px;padding-top: 20px;text-align: center;">
+                                <span style="font-weight: bold;font-size:16px; ;">CHILD WELFARE DIGITAL SERVICES</span><br>
+                                <span style="font-weight: bold;font-size:10px; ;">CALIFORNIA HEALTH & HUMAN SERVICES</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="shadow" style="padding-top:0px"><img src="images/null.png"/></div>
+    <div id="body">
+        <div class="wrapper" style="min-height:500px;">
+            <div class="form-group col-xs-4 AppNoNewRow" style="min-height:inherit;">
+                <div class="panel panel-default" id="divmessagepanel" style="min-height:inherit;">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">What's new</h3>
+                    </div>
+                    <div class="panel-body">
+                        <p>Welcome to Case Management & Residential Licensing System hosted by California Helath & Human
+                            Services</p>
+                        <p>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group col-xs-4 AppNoNewRow" style="min-height:inherit;">
+                <div class="panel panel-default" id="divlinkpanel" style="min-height:inherit;">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Links</h3>
+                    </div>
+                    <div class="panel-body">
+                        <p><a href="http://www.chhs.ca.gov/" target="_blank">CHHS</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group col-xs-4 AppNoNewRow" style="min-height:inherit;">
+                <div class="panel panel-default" id="divloginpanel" style="min-height:inherit;">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Log in</h3>
+                    </div>
+                    <div class="panel-body" style="float:none;">
+                        <p class="help-block">
+                            This is a State of California computer application that is for official use only by
+                            authorized users and is subject to being monitored and/or restricted at any time. This
+                            application processes, stores, or transmits confidential information. Unauthorized or
+                            improper use of this system may result in administrative disciplinary action and/or Civil
+                            and criminal penalties. By continuing to use this application you indicate your awareness of
+                            and consent to these terms and conditions of use. Do not log in and use this application if
+                            you are not an authorized user or you do not agree to the conditions stated in this warning.<br/><br/>
+                        </p>
+                        <div class="form-group" style="padding-bottom:35px;">
+                            <div class="col-xs-7 col-xs-offset-5">
+                                <a class="btn btn-primary" href="" onclick="return SecurePage();" target="_parent"
+                                   role="button" id="lnkAppLogin">Log In</a>
+                            </div>
+                        </div>
+                        <div class="form-group" style="margin-top:10px;">
+                            <div class="col-xs-11 col-xs-offset-1">
+                                <div style="width:100%;text-align:center;">
+                                    <a href="https://ext2.dhcs.ca.gov/webpassmgr/" target="_blank"
+                                       id="lnkForgotPassword">Forgot Password or Username?</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        <footer>
+            <div class="row-fluid">
+                <div class="col-sm-12 footer">
+                    <p class="footercolor">
+                        © CHHS 2016 | Version <span id="spnVersion">1.0.0.0</span>
+                    </p>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+</body>
+</html>
+<script type="text/javascript">
+
+    $(document).ready(function () {
+
+    });
+
+
+</script>
---
+++ Scripts/portal.custom.js
@@ -0,0 +1,175 @@
+﻿var DefaultDropDownText = "Please select...";
+var DefaultDropDownValue = "0";
+
+var DefaultMessageDropDownText = "ALL";
+var DefaultMessageDropDownValue = "-1";
+
+var IsDefaultDropDownEntryNeeded = true;
+
+//here input ddlInput is of type dropdown
+//please make sure to call this before filling any other entries
+function AppAdminFillDefaultEntryForDropdown(ddlInput) {
+    if ($(ddlInput) != null) {
+        $(ddlInput).html('');
+        $(ddlInput).append("<option value='" + DefaultDropDownValue + "'>" + DefaultDropDownText + "</option>");
+        return ddlInput;
+    }
+}
+
+function AppMessageFillDefaultEntryForDropdown(ddlInput) {
+    if ($(ddlInput) != null) {
+        $(ddlInput).html('');
+        $(ddlInput).append("<option value='" + DefaultMessageDropDownValue + "'>" + DefaultMessageDropDownText + "</option>");
+        return ddlInput;
+    }
+}
+
+function RemoveBSAndTildaFromUrl(inputURL)//url be like Search/RunSearch etc
+{
+    var formattedURL = inputURL;
+
+    while (formattedURL != null && formattedURL.length >= 1 && (formattedURL.substring(0, 1) == '/' || formattedURL.substring(0, 1) == '~' || formattedURL.substring(0, 1) == '.')) {
+        formattedURL = formattedURL.replace(formattedURL.substring(0, 1), '');//replace first occurence
+    }
+
+    return formattedURL;
+}
+
+function getAppAbsoluteUrl(inputPartialUrl) {
+
+    inputPartialUrl = RemoveBSAndTildaFromUrl(inputPartialUrl);
+
+    var baseUrl = window.location.protocol + "//" + window.location.host + "/";
+
+    return baseUrl + inputPartialUrl;
+
+}
+
+function IsEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+}
+
+function setFormStatusMessage(formName, divcontainerName, isSuccess, message) {
+    var container = $('#' + formName).find('#' + divcontainerName);
+    if (container) {
+        var messageDivSuccess = $(container).find('.alert-success');
+        var messageDivError = $(container).find('.alert-danger');
+
+        if (isSuccess) {
+            $(container).show(); $(messageDivSuccess).show(); $(messageDivSuccess).html(message );
+            $(messageDivError).hide();
+        }
+        else {
+            $(container).show(); $(messageDivSuccess).hide();
+            $(messageDivError).show(); $(messageDivError).html(message);
+        }
+    }
+}
+
+function getUrlJsonSync(url) {
+
+    var jqxhr = $.ajax({
+        type: "GET",
+        url: url,
+        dataType: 'json',
+        cache: false,
+        async: false
+    });
+
+    // 'async' has to be 'false' for this to work
+    var response = { valid: jqxhr.statusText, data: jqxhr.responseJSON };
+
+    return response;
+}
+
+function postUrlJsonSync(url) {
+
+    var jqxhr = $.ajax({
+        type: "POST",
+        url: url,
+        dataType: 'json',
+        cache: false,
+        async: false
+    });
+
+    // 'async' has to be 'false' for this to work
+    var response = { valid: jqxhr.statusText, data: jqxhr.responseJSON };
+
+    return response;
+}
+
+function HideFormStatusMessage(formName, divcontainerName) {
+    var container = $('#' + formName).find('#' + divcontainerName);
+    if (container) {
+        $(container).hide();
+    }
+}
+
+function getParameterByName(url, idtofindvaluefor) {
+    idtofindvaluefor = idtofindvaluefor.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + idtofindvaluefor + "=([^&#]*)"),
+        results = regex.exec(url);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function AppShowAjaxLoadingContent(divName) {
+    $('#' + divName).html('<h1 class="ajax-loading-animation" style=\'padding-left:10px;\'><i class="fa fa-cog fa-spin"></i> Loading...</h1>');
+}
+
+function AppShowAjaxLoadingContentSmall(divName) {
+    $('#' + divName).html('<h1 class="ajax-loading-animation-small" style=\'padding-left:10px;\'><i class="fa fa-cog fa-spin"></i> Loading...</h1>');
+}
+
+function Clear_Form(containerName) {
+
+    var eleName = '#' + containerName;
+
+    $(eleName).find(':input').each(function () {
+
+        var isdisabled = $(this).is(':disabled');
+
+        switch (this.type) {
+            case 'password':
+            case 'select-multiple':
+            case 'select-one':
+            case 'text':
+            case 'tel':
+            case 'email':
+            case 'textarea':
+                $(this).val('');
+                break;
+            case 'checkbox':
+            case 'radio':
+                this.checked = false;
+        }
+    });
+
+    //hide form error area
+    $(eleName).find('.AppFormError').hide();
+
+    $(eleName).find('.has-error').each(function () {
+        $(this).find('.has-error').remove();
+        $(this).removeClass('has-error');
+    });
+
+    $(eleName).find('.has-success').each(function () {
+        $(this).removeClass('has-success');
+    });
+}
+
+function AppControlAllowOnlyAlphabets(kCode) {
+    if ((kCode > 64 && kCode < 91) || (kCode > 96 && kCode < 123)) {
+        return true;
+    }
+    else {
+        event.keyCode = 0
+        return false;
+    }
+};
+
+
+$(document).ready(function () {
+});
+
+
